e0fcf4aafc77da92177b15b6e2e37bafc78c75e4
PYRS <yagosousa2512@gmail.com>
2024-02-08 02:17:48 -0300

    Update: change port

--- app/back/src/main.ts
+++ app/back/src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
 	app.use(bodyParser.json({ limit: '50mb' }));
 	app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
-	await app.listen(10000);
+	await app.listen(3001);
 }
 
 bootstrap();
